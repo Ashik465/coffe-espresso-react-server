0a20eefa69fadd6a0840f1c5747f4e55071b3aa3
Ashik465 <helloashikul@gmail.com>
2023-05-10 01:03:02 +0600

    delete done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const  cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -42,6 +42,8 @@ async function run() {
 
       })
 
+      
+
 
 app.post('/coffee', async (req, res) => {
     const coffee = req.body;
@@ -51,7 +53,12 @@ app.post('/coffee', async (req, res) => {
 })
 
 
-
+ app.delete('/coffee/:id', async (req, res) => {
+    const id = req.params.id;
+    const query ={_id: new ObjectId(id)}
+    const result = await coffeeCollection.deleteOne(query);
+    res.json(result);
+})
 
 
 
